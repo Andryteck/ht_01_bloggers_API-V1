215a22cf4e24aeeaad2358d6f1c2aad156185619
Andrei Kulik <andry_teck@mail.ru>
2022-04-17 15:28:33 +0300

    correct mistake

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -13,7 +13,7 @@ export async function runDb() {
         // Connect the client to the server
         await client.connect();
         // Establish and verify connection
-        await client.db("bloggers").command({ ping: 1 });
+        await client.db("bloggers-db").command({ ping: 1 });
         console.log("Connected successfully to mongo server");
 
     } catch {
